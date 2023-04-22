08026f5b7c59f37fc46806bc21ecd2d2710e4e3d
apple <apple@apples-MacBook-Pro.local>
2023-04-23 00:18:31 +0500

    feat(): complete the crud operation for product module

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,17 +1,90 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Res,
+} from "@nestjs/common";
+import { CreateProductDto, UpdateProductDto } from "./product.dto";
 import { ProductDoc } from "./product.schema";
 import { ProductService } from "./product.service";
 
-@Controller('product')
+@Controller("product")
 export class ProductController {
   constructor(private readonly _svc: ProductService) {}
 
   @Post()
   public async createProduct(
-    @Body("name") name: string,
-    @Body("price") price: string,
-    @Body("desc") desc?: string
+    @Body() product: CreateProductDto,
+    @Res() res
   ): Promise<ProductDoc> {
-    return this._svc.createProduct(name, price, desc)
+    const result = await this._svc.createProduct(product);
+
+    return res.status(HttpStatus.CREATED).json({
+      status: "OK",
+      message: "Record Created Successfully",
+      data: result,
+    });
+  }
+
+  @Get()
+  public async getAllProducts(
+    @Res() res
+  ): Promise<ProductDoc> {
+    const result = await this._svc.getAllProducts();
+
+    return res.status(HttpStatus.CREATED).json({
+      status: "OK",
+      message: "Record Created Successfully",
+      data: result,
+    });
+  }
+
+  @Get(":id")
+  public async getProductById(
+    @Param("id") id: string,
+    @Res() res
+  ): Promise<ProductDoc> {
+    const result = await this._svc.getProductById(id);
+
+    return res.status(HttpStatus.OK).json({
+      status: "OK",
+      message: "Record Get Successfully",
+      data: result,
+    });
+  }
+
+  @Delete(":id")
+  public async deleteProductById(
+    @Param("id") id: string,
+    @Res() res
+  ): Promise<ProductDoc> {
+    const result = await this._svc.deleteProductById(id);
+
+    return res.status(HttpStatus.OK).json({
+      status: "OK",
+      message: "Record Deleted Successfully",
+      data: null,
+    });
+  }
+
+  @Patch(":id")
+  public async updateProductById(
+    @Param("id") id: string,
+    @Body() product: UpdateProductDto,
+    @Res() res
+  ): Promise<ProductDoc> {
+    const result = await this._svc.updateProductById(id, product);
+
+    return res.status(HttpStatus.OK).json({
+      status: "OK",
+      message: "Record Updated Successfully",
+      data: result,
+    });
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { CreateProductDto, UpdateProductDto } from "./product.dto";
 import { ProductDoc } from "./product.schema";
 
 @Injectable()
@@ -8,14 +9,72 @@ export class ProductService {
   constructor(
     @InjectModel("Product") private readonly model: Model<ProductDoc>
   ) {}
-  public async createProduct(
-    name: string,
-    price: string,
-    desc: string
-  ): Promise<ProductDoc> {
-    const product = await this.model.create({ name, price, desc });
-    if (!product) {
+
+  public async createProduct(product: CreateProductDto): Promise<ProductDoc> {
+    const alreadyExist = await this.model.findOne({name: product.name})
+
+    if(alreadyExist) {
+      throw new HttpException("product already exists", HttpStatus.BAD_REQUEST);
+    }
+
+    const newProduct = await this.model.create(product);
+    if (!newProduct) {
       throw new HttpException("product not created", HttpStatus.BAD_REQUEST);
+    }
+    return newProduct;
+  }
+
+  public async getAllProducts(): Promise<ProductDoc[]> {
+   
+
+    const products = await this.model.find().exec();
+    if (!products.length) {
+      throw new HttpException("products not found", HttpStatus.NOT_FOUND);
+    }
+    return products;
+  }
+
+  public async getProductById(id: string): Promise<ProductDoc> {
+    if (!id) {
+      throw new HttpException(
+        "id is missing in params",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    const product = await this.model.findById(id);
+    if (!product) {
+      throw new HttpException("product not get", HttpStatus.BAD_REQUEST);
+    }
+    return product;
+  }
+
+  public async deleteProductById(id: string): Promise<ProductDoc> {
+    if (!id) {
+      throw new HttpException(
+        "id is missing in params",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    const product = await this.model.findByIdAndDelete(id);
+    if (!product) {
+      throw new HttpException("product not deleted", HttpStatus.BAD_REQUEST);
+    }
+    return product;
+  }
+
+  public async updateProductById(
+    id: string,
+    data: UpdateProductDto
+  ): Promise<ProductDoc> {
+    if (!id) {
+      throw new HttpException(
+        "id is missing in params",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    const product = await this.model.findByIdAndUpdate(id, data, { new: true });
+    if (!product) {
+      throw new HttpException("product not updated", HttpStatus.BAD_REQUEST);
     }
     return product;
   }
