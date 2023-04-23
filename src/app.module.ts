3d276e6190cff33fdd25b82789430186522777c0
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-04-24 00:33:02 +0500

    feat(): working on user and authorization module (#3)
    * feat(): working on product module and create a schema for product
    
    * feat(): complete the crud operation for product module
    
    * feat(): working on user and authorization module
    
    ---------
    
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
 import { ProductModule } from "./product/product.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb://localhost:27017/amazon"),
     ProductModule,
+    UserModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.enableCors();
   app.setGlobalPrefix(globalPrefix);
   await app.listen(3000);
 }
