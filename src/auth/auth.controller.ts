86619c0698d3ea70885ac7e70b1afb8a630df64d
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-04-25 17:22:31 +0500

    feat(): working on user and auth modules and create a register API (#4)
    * feat(): working on product module and create a schema for product
    
    * feat(): complete the crud operation for product module
    
    * feat(): working on user and authorization module
    
    ---------
    
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpStatus, Post, Res } from "@nestjs/common";
+import { RegisterUserDto } from "src/user/user.dto";
+import { UserDetails } from "src/user/user.interface";
 import { AuthService } from "./auth.service";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly _svc: AuthService) {}
+
+  @Post("register")
+  public async register(
+    @Body() user: RegisterUserDto,
+    @Res() res
+  ): Promise<UserDetails | null> {
+    const result = await this._svc.register(user);
+
+    return res.status(HttpStatus.OK).json({
+      status: "OK",
+      message: "Record Updated Successfully",
+      data: result,
+    });
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { UserDetails } from "./user.interface";
+import { UserDoc } from "./user.schema";
 
 @Injectable()
 export class UserService {
-  constructor(
-  ) {}
+  constructor(@InjectModel("User") private readonly model: Model<UserDoc>) {}
 
- 
+  _getUserDetails(user: UserDoc): UserDetails {
+    return {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+
+  public async findByEmail(email: string): Promise<UserDoc> {
+    return this.model.findOne({ email }).exec();
+  }
+
+  public async findById(id: string): Promise<UserDetails | null> {
+    const user = await this.model.findById(id).exec();
+
+    if (!user) return null;
+
+    return this._getUserDetails(user);
+  }
+
+  public async create(
+    name: string,
+    email: string,
+    hashPassword: string
+  ): Promise<UserDoc> {
+    return this.model.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+  }
 }
